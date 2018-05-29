6bccd6d899b0257a9d8cc1c556b899c946ef67a0
Tad Nikolaeva <tad.nikolaeva@gmail.com>
2018-05-29 19:41:43 +0500

    2 step of auth and verification

--- ui/front/app/components/pages/loginPage/LoginPage.jsx
+++ ui/front/app/components/pages/loginPage/LoginPage.jsx
@@ -8,24 +8,30 @@ import NotificationSystem from 'react-notification-system';
 
 import LoginForm from '../../ui/forms/loginForm/LoginForm';
 import Modal from '../../ui/common/modal/Modal';
+import VerificationForm from '../../ui/forms/verificationForm/VerificationForm';
+import Button from '../../ui/common/button/Button';
 
 import login from '../../../actions/auth/login';
 import redirect from '../../../actions/redirect';
 import checkAuthorized from '../../../actions/auth/checkAuthorized';
 import getUser from '../../../actions/auth/getUser';
+import setTimer from '../../../actions/timer';
 
 import styles from './LoginPage.pcss';
 
 class LoginPage extends PureComponent {
 
     state = {
-        showForgotPasswordModal: false
+        showForgotPasswordModal: false,
+        stage: 1
     };
 
     componentDidMount() {
         this.props.actions.checkAuthorized().then(() => {
             this.props.actions.redirect('/');
         });
+
+        this.props.actions.setTimer(new Date().getTime() + 90000);
     };
 
     handleSetRef = (refName) => component => {
@@ -48,19 +54,23 @@ class LoginPage extends PureComponent {
     };
 
     handleForgetPassword = () => {
-      this.setState({
-          showForgotPasswordModal: true
-      });
+        this.setState({
+            showForgotPasswordModal: true
+        });
     };
 
     handleCloseModal = () => {
-      this.setState({
-          showForgotPasswordModal: false
-      });
+        this.setState({
+            showForgotPasswordModal: false
+        });
+    };
+
+    handleSendRetry = () => {
+        this.props.actions.setTimer(new Date().getTime() + 90000);
     };
 
     render() {
-        const {showForgotPasswordModal} =this.state;
+        const { showForgotPasswordModal, stage } = this.state;
 
         return (
             <I18n ns='translations'>
@@ -68,41 +78,78 @@ class LoginPage extends PureComponent {
                     (t) => (
                         <div className={styles.wrapper}>
                             <div className={styles.closeWrapper}>
-                                <img className={styles.logo} src='/img/close.svg'/>
+                                <img className={styles.logo} src='/img/close.svg' />
                             </div>
-                            <LoginForm
-                                onSubmit={this.handleSignIn}
-                                onClickForgetPassword={this.handleForgetPassword}
-                            />
-                            <div className={styles.socialBlock}>
-                                <p className={styles.socialBlockTitle}>
-                                    {t('loginPage.signInWith')}
-                                </p>
-                                <div className={styles.socialBlockWrapperButton}>
-                                    <button className={styles.socialButton}>
-                                        <img className={styles.socialButtonIcon} src='/img/social/wechat.svg'/>
-                                    </button>
-                                    <button className={styles.socialButton}>
-                                        <img className={styles.socialButtonIcon} src='/img/social/telegram.svg'/>
-                                    </button>
-                                    <button className={styles.socialButton}>
-                                        <img className={styles.socialButtonIcon} src='/img/social/mail.svg'/>
-                                    </button>
-                                </div>
-                            </div>
-                            <div className={styles.wrapperLabel}>
-                                <span className={styles.label}>{t('loginPage.notAccount')}</span>
-                                <Link
-                                    className={styles.linkSignUp}
-                                    to={'/register'}>
-                                    {t('loginPage.signUp')}
-                                </Link>
-                            </div>
-                            <div className={styles.switchLanguage}>
-                                <button className={styles.switchLanguageButton}>
-                                    RU
-                                </button>
-                            </div>
+                            {
+                                stage === 1 && (
+                                    <div className={styles.content}>
+                                        <LoginForm
+                                            onSubmit={this.handleSignIn}
+                                            onClickForgetPassword={this.handleForgetPassword}
+                                        />
+                                        <div className={styles.socialBlock}>
+                                            <p className={styles.socialBlockTitle}>
+                                                {t('loginPage.signInWith')}
+                                            </p>
+                                            <div className={styles.socialBlockWrapperButton}>
+                                                <button className={styles.socialButton}>
+                                                    <img className={styles.socialButtonIcon}
+                                                         src='/img/social/wechat.svg' />
+                                                </button>
+                                                <button className={styles.socialButton}>
+                                                    <img className={styles.socialButtonIcon}
+                                                         src='/img/social/telegram.svg' />
+                                                </button>
+                                                <button className={styles.socialButton}>
+                                                    <img className={styles.socialButtonIcon}
+                                                         src='/img/social/mail.svg' />
+                                                </button>
+                                            </div>
+                                        </div>
+                                        <div className={styles.wrapperLabel}>
+                                            <span className={styles.label}>{t('loginPage.notAccount')}</span>
+                                            <Link
+                                                className={styles.linkSignUp}
+                                                to={'/register'}>
+                                                {t('loginPage.signUp')}
+                                            </Link>
+                                        </div>
+                                        <div className={styles.switchLanguage}>
+                                            <button className={styles.switchLanguageButton}>
+                                                RU
+                                            </button>
+                                        </div>
+                                    </div>)
+                            }
+                            {
+                                stage === 2 && (
+                                    <div className={styles.content}>
+                                        <div className={styles.headerWrapper}>
+                                            <h1 className={styles.header}>{t('loginPage.security.header')}</h1>
+                                            <p className={styles.info}>{t('loginPage.security.description')}</p>
+                                            <p className={styles.warning} dangerouslySetInnerHTML={{ __html: t('loginPage.security.warning')}}/>
+                                            <p className={styles.info}>{t('loginPage.security.comment')}</p>
+                                            <Button
+                                                label={t('loginPage.security.buttonLabel')}
+                                            />
+                                        </div>
+                                    </div>)
+                            }
+                            {
+                                stage === 3 && (
+                                    <div className={styles.content}>
+                                        <div className={styles.headerWrapper}>
+                                            <h1 className={styles.header}>{t('loginPage.verification.header')}</h1>
+                                            <h2 className={styles.title}>{t('loginPage.verification.title')}</h2>
+                                            <p className={styles.comment}>{t('loginPage.verification.comment')} </p>
+                                        </div>
+                                        <VerificationForm
+                                            onSubmit={this.handleSubmitVerificationForm}
+                                            timer={this.props.timer}
+                                            onClickSendRetry={this.handleSendRetry}
+                                        />
+                                    </div>)
+                            }
                             <NotificationSystem ref={this.handleSetRef('notification')} />
                             {
                                 showForgotPasswordModal && (
@@ -125,10 +172,14 @@ class LoginPage extends PureComponent {
     }
 }
 
-function mapStateToProps() {
+function mapStateToProps(state, props) {
+    const { timer: { timer } } = state;
 
-    return {};
+    return {
+        timer
+    };
 }
+
 
 function mapDispatchToProps(dispatch) {
     return {
@@ -136,7 +187,8 @@ function mapDispatchToProps(dispatch) {
             login,
             redirect,
             checkAuthorized,
-            getUser
+            getUser,
+            setTimer
         }, dispatch)
     };
 }
